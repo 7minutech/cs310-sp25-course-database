aea66875a88bbe14564382607765be9f332a4348
7minutech <elijohnson1107@gmail.com>
2025-02-05 18:55:32 -0600

    ptest: converting single record into json string

--- src/edu/jsu/mcis/cs310/coursedb/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/coursedb/dao/DAOUtility.java
@@ -11,13 +11,27 @@ public class DAOUtility {
     public static String getResultSetAsJson(ResultSet rs) {
         
         JsonArray records = new JsonArray();
+        JsonObject studentObj  = new JsonObject();
+        String key,value;
+        int columnCount;
+        
         
         try {
-        
+            // My init commit for repo
+            // INSERT YOUR CODE HERE
+            ResultSetMetaData metadata;
+            metadata = rs.getMetaData();
+            columnCount = metadata.getColumnCount();
+            
             if (rs != null) {
-                // My init commit for repo
-                // INSERT YOUR CODE HERE
-
+                while(rs.next()){
+                    studentObj.put("studentid",rs.getInt("studentid"));
+                    studentObj.put("termid",rs.getInt("termid"));
+                    studentObj.put("crn",rs.getInt("crn"));
+                    records.add(studentObj);
+                }
+                records.toString();
+                
             }
             
         }
