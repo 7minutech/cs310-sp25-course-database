412c3f429a3743ff6bac206f88a42d111945c319
7minutech <elijohnson1107@gmail.com>
2025-02-10 06:07:32 -0600

    fix: moved return statement

--- src/edu/jsu/mcis/cs310/coursedb/dao/SectionDAO.java
+++ src/edu/jsu/mcis/cs310/coursedb/dao/SectionDAO.java
@@ -1,9 +1,14 @@
 package edu.jsu.mcis.cs310.coursedb.dao;
 
+import com.github.cliftonlabs.json_simple.JsonArray;
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.*;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.sql.Statement;
 
 public class SectionDAO {
     
@@ -18,11 +23,13 @@ public class SectionDAO {
     public String find(int termid, String subjectid, String num) {
         
         String result = "[]";
-        
+        String key;
         PreparedStatement ps = null;
-        ResultSet rs = null;
+        ResultSet rs = null;    
         ResultSetMetaData rsmd = null;
-        
+        String value;
+        int columnCount;
+        JsonArray records = new JsonArray();
         try {
             
             Connection conn = daoFactory.getConnection();
@@ -30,7 +37,23 @@ public class SectionDAO {
             if (conn.isValid(0)) {
                 
                 // INSERT YOUR CODE HERE
-                
+                ps = conn.prepareStatement(QUERY_FIND, Statement.RETURN_GENERATED_KEYS);
+                ps.setInt(1, termid);
+                ps.setString(2, subjectid);
+                ps.setString(3, num);
+                ps.executeQuery();
+                rs = ps.getResultSet();
+                rsmd = rs.getMetaData();
+                columnCount = rsmd.getColumnCount();
+                while(rs.next()) {
+                    JsonObject sectionObj  = new JsonObject();
+                    for (int i = 1; i <= columnCount; ++i) {
+                        key = rsmd.getColumnLabel(i);
+                        value = rs.getString(i);
+                        sectionObj.put(key,value);
+                    }
+                    records.add(sectionObj);
+                }
             }
             
         }
@@ -44,7 +67,7 @@ public class SectionDAO {
             
         }
         
-        return result;
+        return Jsoner.serialize(records);
         
     }
     
